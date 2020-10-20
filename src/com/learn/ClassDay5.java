d69a6131f06ed6201392d00870609fed625a6c51
Bernice Bonsu <bbonsu@yahoo.com>
2020-10-19 18:03:42 -0600

    initial commit

---
+++ src/com/learn/ClassDay5.java
@@ -0,0 +1,14 @@
+
+package com.learn;
+//Package is how you group your classes
+
+public class ClassDay5 {
+
+    public static void main(String[] args){
+        System.out.println("print me");
+
+    }
+    git config --global user.name "Yaajava"
+    $ git config --global user.email johndoe@example.com
+
+}
