55f1d58e4ff38b41c3fdb8158be39b5a09f2251b
Sam <sacheampong@merchante-solutions.com>
2020-10-19 21:31:10 -0400

    first assignment

---
+++ src/com/learn/Assignment1.java
@@ -0,0 +1,30 @@
+package com.learn;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+
+	}
+
+
+	/*
+	Write a program to calculate the sum of first 10 natural number.
+	 */
+
+
+
+	/*
+	Write a program to print Fibonacci series of n terms where n can be any number:
+		0 1 1 2 3 5 8 13 24 .....
+	 */
+
+
+
+	/*
+ 	 Write a program that prints the following on the screen
+	 **********
+	 **********
+	 **********
+	 **********
+	 */
+}
